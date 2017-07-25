a58dcfe57a5f676484f318e64add19db0f887231
Joaquin Ignacio <Keen@Revolt.local>
2017-07-24 19:34:25 -0700

    updated to do both listens in one file within the serverGOODjs file

--- serverGOOD.js
+++ serverGOOD.js
@@ -2,23 +2,36 @@
 var http = require("http");
 
 // Here we define a port to listen to
-var PORT = 7000;
+var PORTgood = 7000;
+var PORTbad = 7500;
 
 // Here we create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequestGood(request, response) {
 
   // The below statement is triggered (client-side) when the user visits the PORT URL
   response.end("You're beautiful on the inside and outside.");
 }
 
-// Here we use the Node HTTP package to create our server.
-// We then pass it the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+function handleRequestBad(request, response) {
+
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("You're hideous on the inside and outside.");
+}
+
+var serverGood = http.createServer(handleRequestGood);
+var serverBad = http.createServer(handleRequestBad);
 
 // Here we start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+serverGood.listen(PORTgood, function() {
 
   // The below statement is triggered (server-side) when a user visits the PORT URL
   console.log("You are a brilliant full stack developer!");
+
+});
+
+serverBad.listen(PORTbad, function() {
+
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("You are a hideous full stack developer!");
 
 });
